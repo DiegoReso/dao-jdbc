2149f8dd87b41a8c680d0dbbb665b91d781754c9
Diego Reis <diegoreso@me.com>
2025-05-07 09:10:25 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,15 +2,21 @@ package application;
 
 import db.*;
 import model.entities.Department;
+import model.entities.Seller;
 
 
 import java.sql.*;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1,"Books");
         System.out.println(obj);
+
+        Seller seller = new Seller(1,"Diego Reis", "diego@me.com", new Date(), 3000.0 , obj);
+
+        System.out.println(seller);
 
     }
 }
