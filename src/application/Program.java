a60cda098004e7b4ee38fd939367aacd1200380d
Diego Reis <diegoreso@me.com>
2025-05-07 14:53:55 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,18 +21,34 @@ public class Program {
         System.out.println(seller);
 
         System.out.println();
-        System.out.println("=== TEST 1: seller findByDepartment ===");
+        System.out.println("=== TEST 2: seller findByDepartment ===");
         List<Seller> sellerList = sellerDao.findByDepartment(new Department(1, "Computer"));
         for (Seller s: sellerList){
             System.out.println(s);
         }
 
         System.out.println();
-        System.out.println("=== TEST 1: seller findAll ===");
+        System.out.println("=== TEST 3: seller findAll ===");
         sellerList = sellerDao.findAll();
         for (Seller s: sellerList){
             System.out.println(s);
         }
+
+        Department newDepartment = new Department(2, null);
+
+        System.out.println();
+        System.out.println("=== TEST 4: seller insert ===");
+        Seller newSeller =new Seller(null, "Greg", "greg@email.com", new Date(), 4000.0, newDepartment);
+        sellerDao.insert(newSeller);
+        System.out.println("Adicionado novo id : " + newSeller.getId());
+
+
+        System.out.println();
+        System.out.println("=== TEST 5: seller update ===");
+        Seller sellerUpdate = sellerDao.findById(1);
+        sellerUpdate.setName("Willian Estromboloffe");
+        sellerDao.update(sellerUpdate);
+        System.out.println("Atualizado no banco de dados!");
 
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -7,10 +7,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import javax.swing.plaf.SliderUI;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,13 +22,68 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     @Override
-    public void insert(Seller obj) {
+    public void insert(Seller seller) {
 
+        PreparedStatement st = null;
+
+        try {
+            st = conn.prepareStatement("INSERT INTO seller " +
+                    "(name, email, birthdate, base_salary, departmentId) " +
+                    "VALUES " +
+                    "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, seller.getName());
+            st.setString(2,seller.getEmail());
+            st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+            st.setDouble(4, seller.getBaseSalary());
+            st.setInt(5, seller.getDepartment().getId());
+
+
+            int rowsAffected = st.executeUpdate();
+
+            if(rowsAffected > 0 ){
+                ResultSet rs = st.getGeneratedKeys();
+                 if(rs.next()){
+                     int id = rs.getInt(1);
+                     seller.setId(id);
+                 }
+                DB.closeResultSet(rs);
+            }else{
+                throw new DbException("Unexpected error! No rows affected");
+            }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
-    public void update(Seller obj) {
+    public void update(Seller seller) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement("UPDATE seller " +
+                            "SET name = ?, email = ?, birthdate = ?, base_salary = ?, departmentId = ? " +
+                            "WHERE id = ? ",
+                    Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, seller.getName());
+            st.setString(2,seller.getEmail());
+            st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+            st.setDouble(4, seller.getBaseSalary());
+            st.setInt(5, seller.getDepartment().getId());
+            st.setInt(6, seller.getId());
+
+
+            st.executeUpdate();
+
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
